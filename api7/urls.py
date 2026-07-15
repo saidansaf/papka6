6dd0462f026aed2be829572631cc823509feb991
bonu <abdurazakovazarifa02gmail.com>
2026-07-15 13:27:47 +0500

    jnerjnfe

--- api7/urls.py
+++ api7/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet
-
-router = DefaultRouter()
-
-router.register('products', ProductViewSet)
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include(router.urls))
+    path("products/", views.ProductListAPIView.as_view(), name="product-list"),
+    path("products/create/", views.ProductCreateAPIView.as_view(), name="product-create"),
+    path("products/<int:pk>/", views.ProductRetrieveAPIView.as_view(), name="product-detail"),
+    path("products/update/<int:pk>/", views.ProductUpdateAPIView.as_view(), name="product-update"),
+    path("products/delete/<int:pk>/", views.ProductDestroyAPIView.as_view(), name="product-delete"),\
+    path("products/detail-update/<int:pk>/", views.ProductRetrieveAPIView.as_view(), name="product-detail-update"),
+
 ]
--- api7/views.py
+++ api7/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ProductSerializers
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView, RetrieveUpdateAPIView
+from .models import Product
 
-# Create your views here.
+class ProductListAPIView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializers
+
+
+class ProductCreateAPIView(CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializers
+
+
+class ProductRetrieveAPIView(RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializers
+
+
+class ProductUpdateAPIView(UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializers
+
+
+class ProductDestroyAPIView(DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializers
+
+class ProductRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializers
