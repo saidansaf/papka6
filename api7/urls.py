34daad4fa21b67114fb4f9d8fbf5f7d12b05f934
saidansaf <afzalxonovsaidansaf@gmail.com>
2026-07-16 22:12:07 +0500

    gjtknegjtkjr

--- api7/urls.py
+++ api7/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("products/", views.ProductListAPIView.as_view(), name="product-list"),
-    path("products/create/", views.ProductCreateAPIView.as_view(), name="product-create"),
-    path("products/<int:pk>/", views.ProductRetrieveAPIView.as_view(), name="product-detail"),
-    path("products/update/<int:pk>/", views.ProductUpdateAPIView.as_view(), name="product-update"),
-    path("products/delete/<int:pk>/", views.ProductDestroyAPIView.as_view(), name="product-delete"),\
-    path("products/detail-update/<int:pk>/", views.ProductRetrieveAPIView.as_view(), name="product-detail-update"),
-
+    path("api/", views.ProductListAPIView.as_view()),
+    path("api/<int:pk>/", views.ProductRetrieveAPIView.as_view()),
+    path("api/create/", views.ProductCreateAPIView.as_view()),
+    path("api/update/<int:pk>/", views.ProductUpdateAPIView.as_view()),
+    path("api/delete/<int:pk>/", views.ProductDestroyAPIView.as_view()),
+    path("api/", views.ProductListCreateAPIView.as_view()),
+    path("api/<int:pk>/", views.ProductRetrieveUpdateAPIView.as_view()),
+    path("api/<int:pk>/", views.ProductRetrieveDestroyAPIView.as_view()),
+    path("api/<int:pk>/", views.ProductRetrieveUpdateDestroyAPIView.as_view()),
 ]
